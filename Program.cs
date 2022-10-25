28d5211b277b2261fe7e596f05386d2f33cf0b51
Alexandr <jwchaganav@gmail.com>
2022-10-25 11:10:26 +0400

    SelectListCommand

--- Program.cs
+++ Program.cs
@@ -1,35 +1,26 @@
-﻿// See https://aka.ms/new-console-template for more information
-// В переменной string есть секретное сообщение
-// во второй есть пароль
-// Пользователю дается 3 попытки ввести пароль и увидеть секретное сообщение
-// ****
-// Добавим команды
-// SetName - установить имя,
-// Help - вывести список команд,
-// SetPassword - установить пароль,
-// Exit - выход из программы,
-// WriteName - вывести имя после ввода пароля
-// ****
-string secret = "Вы угадали пароль, возьмите с полки пряник!";
-string pass = "123";
-string inputUser = String.Empty;
-int count = 1;
-int max = 3;
+﻿string inputUser = String.Empty;
+int countTrial = 1;
+int maxTrial = 3;
 
 Console.WriteLine("Введите help для вывода списка доступных команд");
-inputUser = ReadString(count.ToString());
+inputUser = ReadString(countTrial.ToString());
 DetectionCommand(inputUser);
+
+
+// Функции
+string ReadString(string message)
+{
+    Console.WriteLine($"{message}");
+    string? inputUser = Console.ReadLine();
+    return inputUser ?? "null";
+}
 
 void DetectionCommand(string strName)
 {
     switch (strName)
     {
         case "help": 
-            Console.WriteLine("help - вывести список команд");
-            Console.WriteLine("setName - установить имя");
-            Console.WriteLine("setPassword - установить пароль");
-            Console.WriteLine("exit - выход из программы");
-            Console.WriteLine("writeName - вывести имя после ввода пароля");
+            SelectListCommand();
             break;
         case "setName":
             break;
@@ -40,14 +31,16 @@ void DetectionCommand(string strName)
         case "writeName":
             break;
         default:
-            Console.WriteLine($"Команды {} нет!");
+            Console.WriteLine($"Команды {strName} нет!");
             break;
     }
 }
 
-string ReadString(string message)
+void SelectListCommand()
 {
-    Console.WriteLine($"Попытка {message}");
-    string? inputUser = Console.ReadLine();
-    return inputUser ?? "null";
+    Console.WriteLine("help - вывести список команд");
+    Console.WriteLine("setName - установить имя");
+    Console.WriteLine("setPassword - установить пароль");
+    Console.WriteLine("exit - выход из программы");
+    Console.WriteLine("writeName - вывести имя после ввода пароля");
 }
